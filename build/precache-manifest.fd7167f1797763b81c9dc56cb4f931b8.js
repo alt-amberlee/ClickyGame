7463dd884ac3fae9b231d8724ea706def6372a65
amberlee <amberleeha@gmail.com>
2019-10-31 23:19:21 -0700

    created new working repo

---
+++ build/precache-manifest.fd7167f1797763b81c9dc56cb4f931b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f3767b344766912da9d2a2ae95a7d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45dac96095352fcc2bbd",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e6effbca24685ced80aa",
+    "url": "/static/js/2.61f2fa50.chunk.js"
+  },
+  {
+    "revision": "45dac96095352fcc2bbd",
+    "url": "/static/js/main.cbcbedf7.chunk.js"
+  },
+  {
+    "revision": "1f1e643385e17360f83c",
+    "url": "/static/js/runtime-main.c2d72319.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
